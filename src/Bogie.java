6067b0a52150a253b2be2cc25a00d761b6c82a48
Wild-Dandelion <sm5756@srmist.edu.in>
2026-04-07 09:12:37 +0530

    Fix: Add Bogie class required for UC7

---
+++ src/Bogie.java
@@ -0,0 +1,15 @@
+public class Bogie {
+        String name;
+        int capacity;
+
+        public Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+
+        @Override
+        public String toString() {
+            return name + " (Capacity: " + capacity + ")";
+        }
+}
+
